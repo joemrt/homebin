10869158acd617e30e20da2f4cdb53f3e8f66b79
Joerg Martin <goergi@gmx.net>
2020-10-25 10:20:33 +0100

    Avoid need for temporary file

--- create_news_html.py
+++ create_news_html.py
@@ -7,12 +7,11 @@ number_of_entries = 3
 
 feed_entries = feedparser.parse(url).entries
 
-with open('/tmp/arch_news.html','w') as f:
-    f.write('<h1> News feed of wwww.archlinux.org</h1>')
-    for i, e in enumerate(feed_entries):
-        if i >= number_of_entries:
-            break
-        f.write('<h2>'+ e.title + '</h2>\n')
-        f.write(e.description + '\n')
-        f.write(3*'<br>&nbsp;<br>\n')
+print('<h1> News feed of wwww.archlinux.org</h1>')
+for i, e in enumerate(feed_entries):
+    if i >= number_of_entries:
+        break
+    print('<h2>'+ e.title + '</h2>\n')
+    print(e.description + '\n')
+    print(3*'<br>&nbsp;<br>\n')
 
